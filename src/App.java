454154eb439f72f5486debcead35ef71e1c9a4a6
Jzavala <jzavalae@est.ups.edu.ec>
2024-07-10 18:03:53 -0500

    Clase Programacion Dinámica

--- src/App.java
+++ src/App.java
@@ -1,23 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Programación Dinámica");
 
-        long startime, endtime = 0;
+        float startime, endtime = 0;
         startime = System.nanoTime();
         System.out.println(fibonacci(40));
         endtime = System.nanoTime();
-        System.out.println("Time taken: " + (endtime - startime) + " ns");
+        System.out.println(String.format("Time taken: %.9f nanosegundos",
+        endtime - startime));
+        System.out.println(String.format("Time taken: %.9f segundos", 
+        (endtime - startime)/1000000000 ));
 
         startime = System.nanoTime();
         System.out.println(fibonacciWithCaching(40));
         endtime = System.nanoTime();
-        System.out.println("Time taken: " + (endtime - startime) + " ns");
+        System.out.println(String.format("Time taken: %.9f nanosegundos",
+        endtime - startime));
+        System.out.println(String.format("Time taken: %.9f segundos", 
+        (endtime - startime)/1000000000 ));
 
         startime = System.nanoTime();
         System.out.println(fibonacciCaching(40));
         endtime = System.nanoTime();
-        System.out.println("Time taken: " + (endtime - startime) + " ns");
+        System.out.println(String.format("Time taken: %.9f nanosegundos",
+         endtime - startime));
+        System.out.println(String.format("Time taken: %.9f segundos", 
+        (endtime - startime)/1000000000 ));
 
         /// Ejericio 1
         // runEjercicio();
@@ -26,20 +37,42 @@ public class App {
 
     // Normal recursive method
     public static int fibonacci(int n) {
-        // TODO: Implement this method
-        return 0;
+        if (n<=1 ){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 
     // First caching method: Using a Map
+    private static Map<Integer, Integer> cacheM = new HashMap<>();
     public static int fibonacciWithCaching(int n) {
-        // TODO: Implement this method
-        return 0;
+        if (n>=1){
+            return n;
+        }
+        if(cacheM.containsKey(n)){
+            cacheM.get(n);
+        }
+        int result = fibonacciWithCaching(n-1) + fibonacciWithCaching(n - 2);
+        cacheM.put(n, result);
+        return result; 
     }
 
     // Second caching method: Using an array
     public static int fibonacciCaching(int n) {
-        // TODO: Implement this method
-        return 0;
+        int[] cache = new int [n+1];
+        return fibonacciArreglo(n, cache);
+    }
+
+    private static int fibonacciArreglo(int n, int[] cache) {
+       if (n>= 1){
+        return n;
+       }
+       if(cache[n] !=0){
+        return cache[n];
+       }
+        
+       cache[n] = fibonacciArreglo(n-1, cache) + fibonacciArreglo(n - 2, cache);
+       return cache[n];
     }
 
     public static void runEjercicio() {
